40d6e1bd8a1cdd589b9d4c2e2f700d8bc225bd9d
jackchw <jackchw.uk@gmail.com>
2025-08-11 17:19:25 +0100

    fix: macos should not register ipc twice

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,6 @@ app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    ipcMain.handle("dialog:openFile", handleFileSave);
     createWindow();
   }
 });
